75bc69755982e103026c64c7e29838317290efce
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-07-04 17:21:31 +0330

    add button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ import Link from "next/link";
 export default function Home() {
    return (
       <div className="flex min-h-screen flex-col items-center justify-between p-24">
-         <Link href="/products">products</Link>
+         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <Link href="/products">show all products</Link>
+         </button>
       </div>
    );
 }
