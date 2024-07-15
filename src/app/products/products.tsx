de3808ff2bc3447302dcca72dfd74fe6419a847e
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-07-15 20:45:32 +0330

    add react form & zod for handling form

--- src/app/products/products.tsx
+++ src/app/products/products.tsx
@@ -6,15 +6,38 @@ import Link from "next/link";
 import { getProducts, createProduct } from "@/api";
 import ProductItem from "./productItem";
 import useProduct from "@/hooks/useProduct";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const newProductSchema = z.object({
+   name: z
+      .string()
+      .min(3, { message: "product name must be at least 3 characters" }),
+   price: z.number().min(1, { message: "product price must be at least 1" }),
+});
+
+type NewProduct = z.infer<typeof newProductSchema>;
 
 export default function Products() {
-   const { product: newProduct, handleProductDetail } = useProduct();
-
    const queryClient = useQueryClient();
-
    const { data, isError, isLoading, error } = useQuery({
       queryKey: ["products"],
       queryFn: () => getProducts(),
+   });
+
+   const {
+      register,
+      handleSubmit,
+
+      formState: { errors, isValid, isSubmitting },
+   } = useForm<NewProduct>({
+      resolver: zodResolver(newProductSchema),
+      mode: "onChange",
+      defaultValues: {
+         name: "",
+         price: 0,
+      },
    });
 
    const mutation = useMutation({
@@ -25,10 +48,11 @@ export default function Products() {
       },
    });
 
-   const submitProduct = (e: React.FormEvent) => {
-      e.preventDefault();
+   const handleSubmitProduct = (data: NewProduct) => {
+      // FIX: price should't be string
       mutation.mutate({
-         ...newProduct,
+         price: `${data.price}`,
+         name: data.name,
          id: Date.now().toString(),
       });
    };
@@ -45,32 +69,34 @@ export default function Products() {
             <Link href="/">Home</Link>
          </button>
          <div className="max-w-lg lg:ms-auto mx-auto text-center bg-slate-600 p-4 rounded-xl mb-4">
-            <form onSubmit={submitProduct}>
+            <form onSubmit={handleSubmit(handleSubmitProduct)}>
                <div className="grid md:grid-cols-2 grid-cols-1 gap-6 p-4 ">
                   <input
                      type="text"
                      id="fname"
-                     name="name"
-                     value={newProduct.name}
-                     onChange={handleProductDetail}
+                     {...register("name")}
                      placeholder="name"
                      className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-700 "
                   />
+                  {errors.name && (
+                     <p className="text-red-500">{errors.name.message}</p>
+                  )}
                   <input
                      type="number"
                      id="price"
-                     onChange={handleProductDetail}
-                     value={newProduct.price}
-                     name="price"
+                     {...register("price", { valueAsNumber: true })}
                      placeholder="price"
                      className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-700"
                   />
+                  {errors.price && (
+                     <p className="text-red-500">{errors.price.message}</p>
+                  )}
                </div>
                <button
                   type="submit"
                   className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                >
-                  add product
+                  {mutation.status === "pending" ? "loading..." : "add product"}
                </button>
             </form>
          </div>
