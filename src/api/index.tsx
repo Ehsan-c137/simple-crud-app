631b72dc2d2ef708cb7e40a5a43946eb9c28a889
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-07-15 15:04:45 +0330

    import apis

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,3 +1,8 @@
-import GetProducts from "./getProducts";
+import getProducts from "./getProducts";
+import createProduct from "./createProduct";
+import updateProduct from "./updateProduct";
+import deleteProduct from "./deleteProduct";
+
+export { getProducts, createProduct, updateProduct, deleteProduct };
 
 export const BASE_URL = "https://66859a92b3f57b06dd4d4925.mockapi.io/api";
