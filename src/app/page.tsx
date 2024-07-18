5d4f80f277f16a59f5ea144545b273ab41f38f55
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-07-18 23:04:25 +0330

    add shadcn component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-"use client";
-
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
    return (
       <div className="flex min-h-screen flex-col items-center justify-between p-24">
-         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+         <Button>
             <Link href="/products">show all products</Link>
-         </button>
+         </Button>
       </div>
    );
 }
