93b80a0585178f927d4bd6e69ed9491e46a4830e
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-09-24 11:48:21 +0330

    add sticky position to card in desktop

--- src/app/products/products.tsx
+++ src/app/products/products.tsx
@@ -66,35 +66,42 @@ export default function Products() {
 
    return (
       <div className="p-4 w-full min-h-screen bg-slate-200 flex flex-col lg:flex-row gap-4 ">
-         <Card className="p-4 max-h-80 col-span-1">
+         <Card className="p-4 max-h-80 col-span-1 lg:sticky lg:top-4 lg:left-8">
             <form
                onSubmit={handleSubmit(handleSubmitProduct)}
                className="flex flex-col gap-4 "
             >
                <div className="grid md:grid-cols-2 grid-cols-1 gap-6 p-4 ">
-                  <Input
-                     type="text"
-                     id="fname"
-                     {...register("name")}
-                     placeholder="name"
-                     className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-700 "
-                  />
+                  <div className="flex flex-col">
+                     <Input
+                        type="text"
+                        id="fname"
+                        {...register("name")}
+                        placeholder="name"
+                        className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-700 "
+                     />
 
-                  {errors.name && (
-                     <p className="text-red-500">{errors.name.message}</p>
-                  )}
+                     {errors.name && (
+                        <p className="text-red-500 text-md">
+                           {errors.name.message}
+                        </p>
+                     )}
+                  </div>
+                  <div className="flex flex-col">
+                     <Input
+                        type="number"
+                        min={1}
+                        id="price"
+                        {...register("price", { valueAsNumber: true })}
+                        placeholder="price"
+                     />
 
-                  <Input
-                     type="number"
-                     min={1}
-                     id="price"
-                     {...register("price", { valueAsNumber: true })}
-                     placeholder="price"
-                  />
-
-                  {errors.price && (
-                     <p className="text-red-500">{errors.price.message}</p>
-                  )}
+                     {errors.price && (
+                        <p className="text-red-500 text-md">
+                           {errors.price.message}
+                        </p>
+                     )}
+                  </div>
 
                   <Button type="submit">
                      {mutation.status === "pending"
